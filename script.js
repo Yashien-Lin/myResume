ff1f0e832d8f7ada022bda2341714c247727f06e
Yashien-Lin <58139758+Yashien-Lin@users.noreply.github.com>
2022-02-11 12:19:50 +0800

    second commit

--- script.js
+++ script.js
@@ -3,14 +3,10 @@ const elementA = menuList.querySelectorAll("a");
 const navbar = document.querySelector(".navbar-collapse");
 
 menuList.addEventListener("click", (e) => {
-  // e.preventDefault();
-  console.log("elementA:", elementA);
   navbar.classList.remove("show");
 
   elementA.forEach((item) => {
-    // console.log(item);
     if (item.classList.contains("active")) {
-      console.log("Yes!");
       item.classList.remove("active");
     }
   });
@@ -22,19 +18,16 @@ menuList.addEventListener("click", (e) => {
 const TL_content_height = document.querySelectorAll(".timeline-content");
 if (window.innerWidth > 767) {
   TL_content_height.forEach((item) => {
-    // console.log(item.offsetHeight); //-30px
-    item.setAttribute("style", `margin-top:${item.offsetHeight - 30}px`); //-208
+    item.setAttribute("style", `margin-top:${item.offsetHeight - 30}px`);
   });
 }
 
 window.addEventListener("resize", function () {
-  // console.log("window size:", window.innerWidth);
   TL_content_height.forEach((item) => {
-    // console.log(item.offsetHeight); //-30px
     if (window.innerWidth > 767) {
-      item.setAttribute("style", `margin-top:${item.offsetHeight - 30}px`); //-208
+      item.setAttribute("style", `margin-top:${item.offsetHeight - 30}px`);
     } else {
-      item.removeAttribute("style", `margin-top:${item.offsetHeight - 30}px`); //-208
+      item.removeAttribute("style", `margin-top:${item.offsetHeight - 30}px`);
     }
   });
 });
